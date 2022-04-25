a826e0a95dadb9d30d7ebf6266a75ee45a441cb5
mouse1500 <iberezin1500@gmail.com>
2022-04-25 18:31:17 +0300

    First commit

---
+++ learning_logs/views.py
@@ -0,0 +1,7 @@
+from django.shortcuts import render
+
+# Create your views here.
+
+def index(request):
+    """Домашняя страница приложения learning_logs"""
+    return render(request, 'learning_logs/index.html')
